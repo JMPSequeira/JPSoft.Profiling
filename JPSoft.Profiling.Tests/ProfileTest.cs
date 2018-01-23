6a1d6cf25c7fb9c60dfc0b3e91c1d002e8a60f46
João Palma Sequeira <jmpsequeira@gmail.com>
2018-01-23 22:12:58 +0000

    Added assemblyinfo finished profile and tests

--- JPSoft.Profiling.Tests/ProfileTest.cs
+++ JPSoft.Profiling.Tests/ProfileTest.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading;
+using JPSoft.Profiling;
 using NUnit.Framework;
 
 namespace Tests
@@ -6,16 +9,36 @@ namespace Tests
     public class ProfileTest
     {
         Profile _profile;
-        [SetUp]
-        public void Setup()
-        {
 
+        [SetUp]
+        public void Setup() => _profile = new Profile();
+
+        [Test]
+        public void Miliseconds_DatesSet_ResturnsValidDifference()
+        {
+            var date1 = DateTime.Now;
+
+            Thread.Sleep(100);
+
+            var date2 = DateTime.Now;
+
+            var expected = (date2 - date1).TotalMilliseconds;
+
+            _profile.StartedOn = date1;
+            _profile.EndedOn = date2;
+
+            Assert.AreEqual(expected, _profile.Miliseconds);
         }
 
         [Test]
-        public void Test1()
+        [TestCase(true)]
+        [TestCase(false)]
+        public void IsSuccessful_TrueAndFalse_Expected(bool expected)
         {
-            Assert.Pass();
+            if (!expected)
+                _profile.Exception = new Exception();
+
+            Assert.AreEqual(expected, _profile.IsSuccessful);
         }
     }
 }
--- JPSoft.Profiling/Profile.cs
+++ JPSoft.Profiling/Profile.cs
@@ -9,6 +9,10 @@ namespace JPSoft.Profiling
         public DateTime EndedOn { get; internal set; }
         public double Miliseconds
             =>(EndedOn - StartedOn).TotalMilliseconds;
+        public double MilisecondsPerIteration
+            => Miliseconds / Iterations;
+        public double IterationsPerMilisecond
+            => Iterations / Miliseconds;
         public long Iterations { get; internal set; }
         public string Name { get; internal set; }
         public Exception Exception { get; internal set; }
