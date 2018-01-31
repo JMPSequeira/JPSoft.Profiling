faa3cbd1aa7774517dfaec44a99ed2f54c5cfd17
João Palma Sequeira <jmpsequeira@gmail.com>
2018-01-31 07:32:21 +0000

    added abstract test

--- JPSoft.Profiling/Test.cs
+++ JPSoft.Profiling/Test.cs
@@ -2,27 +2,11 @@ using System;
 
 namespace JPSoft.Profiling
 {
-    public class Test
+    public class Test : AbstractTest<Action>
     {
-        public Test(string name,
-            long iterations,
-            Action code)
-        {
-            Guid = Guid.NewGuid();
-            Name = name;
-            Iterations = iterations;
-            Code = code;
-        }
+        public Test(string name, long iterations, Action code) : base(name, iterations, code) { }
 
-        public Test(string name,
-            long iterations,
-            Action code,
-            TimeSpan timeOut) : this(name, iterations, code) => TimeOut = timeOut;
-
-        public Guid Guid { get; }
-        public string Name { get; }
-        public long Iterations { get; }
-        public Action Code { get; }
-        public TimeSpan TimeOut { get; }
+        public Test(string name, long iterations, Action code, TimeSpan timeOut) : base(name, iterations, code, timeOut) { }
     }
+
 }
